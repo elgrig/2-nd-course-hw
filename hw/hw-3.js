e3d8dc2b0e2bd6452a4c453bd6b94f4f7c60489c
Elena <cominghome125@gmail.com>
2023-12-25 22:19:42 +0300

    домашка 3

---
+++ hw/hw-3.js
@@ -0,0 +1,30 @@
+/* 1. let password = 'qwerty';
+let enterPassword = prompt('Введите пароль');
+if (enterPassword === password) {
+    alert('Пароль введен верно')    
+} else {
+    alert('Пароль введен неправильно')
+}; */
+
+/* 2. let c = 0;
+if (c>0 && c<10) {
+    console.log('Верно')
+} else {
+    console.log('Неверно')
+}; */
+
+
+/* 3. let d = 15;
+let e = 14;
+if (d>100 ||  e>100) {
+    console.log('Верно')
+} else {
+    console.log('Неверно')
+}; */
+
+let a = '2';
+let b = '3';
+// Код выше изменять менять нельзя, чтобы решить задачу исправьте код ниже: 
+alert(a + b);
+
+
