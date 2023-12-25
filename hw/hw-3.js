8f0205efb47a4924aa00282bed6a8957dd8a4663
Elena <cominghome125@gmail.com>
2023-12-25 22:23:00 +0300

    домашнее задание 3

--- hw/hw-3.js
+++ hw/hw-3.js
@@ -24,7 +24,61 @@ if (d>100 ||  e>100) {
 
 let a = '2';
 let b = '3';
-// Код выше изменять менять нельзя, чтобы решить задачу исправьте код ниже: 
-alert(a + b);
+alert(Number(a) + Number(b)); */
 
+/* 5. let monthNumber = 12;
+switch (monthNumber) {
+    case 12:
+    case 1:
+    case 2:
+        console.log('Зима')        
+        break;
+    case 3:
+    case 4:
+    case 5:
+        console.log('Весна')        
+        break;
+    case 6:
+    case 7:
+    case 8:
+        console.log('Лето')        
+        break;
+    case 9:
+    case 10:
+    case 11:
+        console.log('Осень')        
+        break;
+    default: console.log('Такого месяца не сущеcтвует')         
+        break;
+} */
 
+/* 7. let num = prompt('Введите любое число');
+console.log(isNaN(num));
+if (num % 2 === 0) {
+    alert('Число четное')
+} else {
+    alert('Число нечетное')
+} */
+
+/* 8. let clientOS = '0';
+if (clientOS == '0') {
+    console.log('Установите версию приложения для iOS по ссылке')
+} else if (clientOS == '1') {
+    console.log('Установите версию приложения для Android по ссылке')
+} */
+
+let clientOS = '0';
+let clientDeviceYear = '2015';
+if (clientOS == '0') {    
+    if (clientDeviceYear < '2015') {
+        console.log('Установите облегченную версию приложения для iOS по ссылке')
+    } else {
+        console.log('Установите версию приложения для iOS по ссылке')
+    }
+} else if (clientOS == '1') {
+    if (clientDeviceYear < '2015') {
+        console.log('Установите облегченную версию приложения для Android по ссылке')
+    } else {
+    console.log('Установите версию приложения для Android по ссылке')  
+    } 
+} 
