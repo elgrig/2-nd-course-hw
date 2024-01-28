60b24c0de32004b461d35169c2811ab7e97eb809
Elena <cominghome125@gmail.com>
2024-01-28 22:45:28 +0300

    ДЗ №7

--- script.js
+++ script.js
@@ -16,4 +16,24 @@ function checkSeason(userNumber) {
         alert('game over. Попробуйте ещё раз!');
         console.log('game over. Попробуйте ещё раз!');
     }
-}
+};
+
+function rememberWords(userAnswerFirst, userAnswerLast) {
+    let list = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
+    list = list.sort(() => Math.random() - 0.5);
+
+    list.forEach((el) => {
+        alert(el);
+    })
+           
+    userAnswerFirst = prompt('Чему равнялся первый элемент массива?');
+    userAnswerLast = prompt('Чему равнялся последний элемент массива?');
+
+    if (userAnswerFirst.toLowerCase() === list[0].toLowerCase() && userAnswerLast.toLowerCase() === list[6].toLowerCase()) {
+        alert('Поздравляем! Вы угадали!');
+    } else if (userAnswerFirst.toLowerCase() === list[0].toLowerCase() || userAnswerLast.toLowerCase() === list[6].toLowerCase()) {
+        alert('Вы были близки к победе!');    
+    } else {
+        alert('Ответ неверный. Попробуйте еще раз!');
+    }
+};
